537c9f313d3428421156a41f2d9f15fef75e570c
mykyda <nikita.danech@gmail.com>
2025-02-05 12:37:41 +0200

    init

---
+++ src/test/java/com/mykyda/symon/SymonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykyda.symon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
